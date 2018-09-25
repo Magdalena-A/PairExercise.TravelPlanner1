f3276705b3c196c307b0ec0e2fa3525b6e5a4c7a
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-09-25 16:22:21 -0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //entry: './src/index.js',
+  //entry defaults to src so not necessary to specify
+  output: {
+    path: __dirname + '/public',
+    filename: "bundle.js"
+  }
+}
